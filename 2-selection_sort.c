ba8f5f0bfbea67c19eb46d6c4f8d9cc41da3b062
Derrick Ampire <ampire90@gmail.com>
2021-10-26 14:13:46 -0700

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of int in ascending order using
+ * Selection sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
