39e0900e2dcd11214f5dc251462b3facf193918c
Derrick Ampire <ampire90@gmail.com>
2021-10-27 11:31:13 -0700

    Shell sort Algo

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -12,7 +12,7 @@ void shell_sort(int *array, size_t size)
 	size_t i, j, gap = 1;
 	int tmp;
 
-	if (array == NULL || size > 2)
+	if (array == NULL || size < 2)
 		return;
 
 	while (gap < size / 3)
